2cce7c77ee462ac8835e6696a8e8ba40454633cd
VanshikaSharma2401 <bulbulsharma2401@gmail.com>
2024-03-07 23:15:09 +0530

    changed profile

--- src/Components/Teamcard.jsx
+++ src/Components/Teamcard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import profile from "../../public/profile.svg";
+// import profile from "../../public/profile.svg";
 
 const Teamcard = ({selected}) => {
   return (
     <div className={`flex gap-2 p-2 px-6 items-center ${selected ? "bg-[#E3B748] rounded-lg ":""}`}>
-      <Image src={profile} />
+      <Image src={""} />
       <div className="flex flex-col gap-1 text-white">
         <p>Danial Def</p>
         <p>301 Cases</p>
